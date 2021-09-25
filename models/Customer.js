d2a76bcb5eae1c39a722317f1d809193465e4082
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-25 14:12:30 +0300

    Added Customer Schema

---
+++ models/Customer.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'),
+const timestamp = require('mongoose-timestamp');
+
+const CustomerSchema = new mongoose.Schema({ //bildiğimiz model..
+    name: {
+        type: String,
+        required: true,
+        trim: true, //kullanıcı girişindeki boşlukların otomatik kırpılması
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true, //kullanıcı girişindeki boşlukların otomatik kırpılması
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    }
+})
+
+CustomerSchema.plugin(timestamp);
+
+const Customer = mongoose.model('Customer', CustomerSchema);
+module.exports = Customer;
