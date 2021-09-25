2b4e9de184dc614c8976d30d3d04da5ea1b79edb
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-25 13:52:44 +0300

    Added customer route for testing

---
+++ routes/customers.js
@@ -0,0 +1,9 @@
+const errors = require('restify-errors');
+
+module.exports = server => {
+    server.get('/customers', (req, res, next) => {
+        res.send({ msg: 'test' });
+
+        next(); //sonraki route'a geçmesi için.
+    })
+}
