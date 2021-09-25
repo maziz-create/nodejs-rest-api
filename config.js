0a67b589ad9e205bfceeb82b0fa34ee56ef3304e
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-25 13:32:08 +0300

    Added environment variable defines

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || '3000',
+    URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://00aziz:<t265W-A4Sv!yX_F>@customers-api.tsoko.mongodb.net/customers-api?retryWrites=true&w=majority'
+}
