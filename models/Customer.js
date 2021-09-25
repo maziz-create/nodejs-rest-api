2c83f0b39177227ffebc0a909c9c8e6b0e9d1051
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-25 14:43:04 +0300

    Completed GetCustomers route in customers.js(Route). Added async-await structure for asynchoronously fetching. Errors is handled with restify-errors module. Deleted a comma in Customer.js(Model) because it occured error.

--- models/Customer.js
+++ models/Customer.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose'),
+const mongoose = require('mongoose');
 const timestamp = require('mongoose-timestamp');
 
 const CustomerSchema = new mongoose.Schema({ //bildiğimiz model..
--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,17 @@
-const errors = require('restify-errors');
+const errors = require('restify-errors'); //hatayı restify-errors ile gönderiyoruz.
+const Customer = require('../models/Customer');
 
 module.exports = server => {
-    server.get('/customers', (req, res, next) => {
-        res.send({ msg: 'test' });
-
-        next(); //sonraki route'a geçmesi için.
-    })
+    //Get Customers
+    server.get('/customers', async (req, res, next) => { //bu fetch işlemi tüm customers'i dönecek.
+        // res.send({ msg: 'test' }); //for testing
+        try {
+            const customers = await Customer.find({}); //önce customers'in gelmesini bekle sonra değişkene ata.
+            //find({}) => collectionda yer alan tüm kayıtları döndür.
+            res.send(customers);
+            next(); //sonraki route'a geçmesi için.
+        } catch (err) {
+            next(new errors.InvalidContentError(err));
+        }
+    });
 }
