568780d9ab57ec9aeb5bf21be43bef7a4df30372
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-10-01 19:51:53 +0300

    Added JWT_SECRET global variable.

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     PORT: process.env.PORT || '3000',
     URL: process.env.BASE_URL || 'http://localhost:3000',
     // MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://00aziz:3363217433@customers-api.tsoko.mongodb.net/customers-api?retryWrites=true&w=majority'
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret1', // secrete verilen değer önemsiz.
 }
